69ac4755b3f49b8502023fcbea7de5f238987839
dhanesh1232 <dhaneshreddy980@gmail.com>
2025-12-29 14:37:41 +0530

    feat(auth): add auth layout component to check session

---
+++ src/app/phoenix-admin-panel-9753/auth/layout.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { data: session } = useSession();
+  if (session?.user) {
+    return;
+  }
+  return children;
+}
